dda881e8f846900d91b22674fa506e13c235ebd2
Heikey <joaofialho123123@gmail.com>
2025-03-07 21:20:44 -0300

    Now he builds!

--- build.zig
+++ build.zig
@@ -1,15 +1,22 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
         .name = "main",
-        .root_source_file = b.path("./src/main.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    const run_step = b.step("run", "run the exectuable.");
-    run_step.dependOn(&exe.step);
+
     b.installArtifact(exe);
+
+    var run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step("run", "run the exectuable.");
+    run_step.dependOn(&run_cmd.step);
+
+
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+## An personal recreational project to use with a future project
+
+This project is being made in the conforms of this [offical document (RFC6455)](https://datatracker.ietf.org/doc/html/rfc6455).
+
+I don't recommend using this in production, this doens't work yet btw.
